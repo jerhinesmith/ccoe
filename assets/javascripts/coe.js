edcc3e2d0298dcc58fcfd9a82505d60b8e53edfe
Justin Rhinesmith <justin@justinrhinesmith.com>
2013-05-27 19:20:25 -0700

    Add the username and story title

--- assets/javascripts/coe.js
+++ assets/javascripts/coe.js
@@ -1,5 +1,12 @@
 function displayComment(comment) {
-  $("#comments").append("<div class='comment'><div class='body'>" + comment.body + "</div><div class='link'><a href='" + comment.location + "'>" + comment.location + "</a></div></div>");
+  $("#comments").append(
+    "<div class='comment'><div class='body'>" +
+    comment.body +
+    "</div><div class='link'><a href='" +
+    comment.location + "'>" +
+    comment.story_title +
+    "</a> - " + comment.user.username + "</div></div>"
+  );
 }
 
 $(document).ready(function () {
